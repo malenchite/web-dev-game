e7d54918362c71395d15d249f69f62c5f4d92504
Steven Israel <malenchite@gmail.com>
2021-04-26 21:55:56 -0400

    Added basic socket.io logic with connected client logging

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 const reactRoutes = require('./routes/reactRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 
+/* Create server objects */
 const app = express();
 
 /* Middleware */
@@ -18,4 +21,35 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(apiRoutes);
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+/* Socket behavior */
+const server = http.createServer(app);
+
+/* Apply CORS for localhost if app is not deployed */
+const io =
+  socketIo(server, process.env.REACT_APP_DEPLOYED ? {} : {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST']
+    }
+  });
+
+const connectedClients = [];
+
+io.on('connection', socket => {
+  console.log('New client connected');
+
+  connectedClients.push(socket.id);
+
+  console.log(connectedClients);
+
+  /* On disconnect, remove ID from list of connected clients */
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+    const index = connectedClients.indexOf(socket.id);
+    if (index > -1) {
+      connectedClients.splice(index, 1);
+    }
+  });
+});
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
