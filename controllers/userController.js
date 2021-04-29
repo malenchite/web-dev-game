3ffe784f6d130f722b66bacb3a7a84389d9e195e
Christian Wittorp <cwwittor@ncsu.edu>
2021-04-28 20:49:12 -0400

    added controller

---
+++ controllers/userController.js
@@ -0,0 +1,55 @@
+const db = require("../models");
+
+// The different methods that are utilized by user controller
+module.exports = {
+    //gets the user fata
+  getUser: (req, res, next) => {
+    if (req.user) {
+      return res.json({ user: req.user });
+    } else {
+      return res.json({ user: null });
+    }
+  },
+    //controller for registering the user
+  register: (req, res) => {
+    const { firstName, lastName, username, password } = req.body;
+    db.User.findOne({ 'username': username }, (err, userMatch) => {
+      if (userMatch) {
+        return res.json({
+          error: `Please select a different username, we already have a user with that name: ${username}`
+        });
+      }
+      const newUser = new db.User({
+        'firstName': firstName,
+        'lastName': lastName,
+        'username': username,
+        'password': password
+      });
+      newUser.save((err, savedUser) => {
+        if (err) return res.json(err);
+        return res.json(savedUser);
+      });
+    });
+  },
+    //logs the user out
+  logout: (req, res) => {
+    if (req.user) {
+      req.session.destroy();
+      res.clearCookie('connect.sid'); 
+      return res.json({ msg: 'logging you out' });
+    } else {
+      return res.json({ msg: 'no user to log out!' });
+    }
+  },
+  auth: function(req, res, next) {
+		next();
+  },
+  authenticate: (req, res) => {
+		const user = JSON.parse(JSON.stringify(req.user)); 
+		const cleanUser = Object.assign({}, user);
+		if (cleanUser) {
+			delete cleanUser.password;
+		}
+		res.json({ user: cleanUser });
+	}
+};
