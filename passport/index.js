25475aaebf6930e85f7d3ccecea340fd8badfceb
Monica Hill <monica.r.hill@gmail.com>
2021-04-28 20:39:30 -0400

    add index.js file to passport folder

---
+++ passport/index.js
@@ -0,0 +1,29 @@
+const passport = require('passport');
+const LocalStrategy = require('./localStrategy');
+const db = require('../models'); // change name once file established
+
+passport.serializeUser((user, done) => {
+// console.log ('Serialize called');
+// console.log(user); // whole raw user object
+
+  done(null, { _id: user._id });
+});
+
+passport.deserializeUser((id, done) => {
+  db.User.findOne(
+    { _id: id },
+    'firstName lastName username',
+    (err, user) => {
+      if (err) {
+        console.log(err);
+      }
+
+      done(null, user);
+    }
+  );
+});
+
+// Register Strategies
+passport.use(LocalStrategy);
+
+module.exports = passport;
