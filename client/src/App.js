aacdab9ed46ff9cd21e1b01d46e313ea99b1d843
Chris Risseler <chrisrisseler43@gmail.com>
2021-04-29 22:12:25 -0400

    added some details to check for login. Also router is browserrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,67 @@
 import './App.css';
-import { HashRouter, Switch, Route } from "react-router-dom";
+// import { HashRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import SocketTest from './pages/SocketTest';
+import LoginForm from "./pages/login"
+import SignupForm from "./pages/register"
 
 function App() {
-  return (
-    <HashRouter>
-      <Switch>
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    AUTH.getUser().then(response => {
+      // console.log(response.data);
+      if (!!response.data.user) {
+        setLoggedIn(true);
+        setUser(response.data.user);
+      } else {
+        setLoggedIn(false);
+        setUser(null);
+      }
+    });
+
+    return () => {
+      setLoggedIn(false);
+      setUser(null);
+    };
+  }, []);
+
+  const logout = (event) => {
+    event.preventDefault();
+
+    AUTH.logout().then(response => {
+      // console.log(response.data);
+      if (response.status === 200) {
+        setLoggedIn(false);
+        setUser(null);
+      }
+    });
+  };
+
+  const login = (username, password) => {
+    AUTH.login(username, password).then(response => {
+      console.log(response.data);
+      if (response.status === 200) {
+        // update the state
+        setLoggedIn(true);
+        setUser(response.data.user);
+      }
+    });
+  };
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <LoginForm />
+        </Route>
+        <Route path="/signup">
+          <SignupForm />
+        </Route>
         {process.env.REACT_APP_DEPLOYED ? "" : <Route path="/socket" component={SocketTest} />}
       </Switch>
-    </HashRouter>
+    </Router>
   );
 }
 
