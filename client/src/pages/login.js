9cedfa0645b4397eef5700d0e5f9337c4f6bbdf0
slisk <63676970+smlisk0630@users.noreply.github.com>
2021-04-30 15:50:06 -0400

    Adding new code for modal component

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,65 +1,150 @@
-import React, { useState } from 'react';
-import { Redirect, Link } from 'react-router-dom';
-import { Container, Row, Col } from '../components/Grid';
-import { Card } from '../components/Card';
-import { Input, FormBtn } from '../components/Form';
+import React, { useState } from "react";
+import { Redirect, Link } from "react-router-dom";
+import { Container, Row, Col } from "../components/Grid";
+import { Card } from "../components/Card";
+import { Input, FormBtn } from "../components/Form";
 // import NewIndex from '../components/Login/newindex'
 
 function LoginForm({ login }) {
-    const [userObject, setUserObject] = useState({
-        username: '',
-        password: ''
+  const [open, setOpen] = useState(true);
+
+  const cancelButtonRef = useRef();
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        static
+        className="fixed z-10 inset-0 overflow-y-auto"
+        initialFocus={cancelButtonRef}
+        open={open}
+        onClose={setOpen}
+      >
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+              <div>
+                <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+                  <CheckIcon
+                    className="h-6 w-6 text-green-600"
+                    aria-hidden="true"
+                  />
+                </div>
+                <div className="mt-3 text-center sm:mt-5">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg leading-6 font-medium text-gray-900"
+                  >
+                    Register or Sign Into the Web Dev Game
+                  </Dialog.Title>
+                </div>
+              </div>
+              <div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+                <button
+                  type="button"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
+                  onClick={() => setOpen(false)}
+                >
+                  Register
+                </button>
+                <button
+                  type="button"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm"
+                  onClick={() => setOpen(false)}
+                  ref={cancelButtonRef}
+                >
+                  Sign In
+                </button>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+
+ // Login code
+  const [userObject, setUserObject] = useState({
+    username: "",
+    password: "",
+  });
+  const [redirectTo, setRedirectTo] = useState(null);
+
+  const handleChange = (event) => {
+    setUserObject({
+      ...userObject,
+      [event.target.name]: event.target.value,
     });
-    const [redirectTo, setRedirectTo] = useState(null);
+  };
 
-    const handleChange = (event) => {
-        setUserObject({
-            ...userObject,
-            [event.target.name]: event.target.value
-        });
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    login(userObject.username, userObject.password);
+    setRedirectTo("/");
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        login(userObject.username, userObject.password);
-        setRedirectTo('/');
-    };
-
-    if (redirectTo) {
-        return <Redirect to={{ pathname: redirectTo }} />
-    } else {
-        return (
-            <Container>
-                <Row>
-                    <Col size="md-3"></Col>
-                    <Col size="md-6">
-                        <Card title="Login to The Web Dev Game">
-                            <form style={{ marginTop: 10 }}>
-                                <label htmlFor="username">Username: </label>
-                                <Input
-                                    type="text"
-                                    name="username"
-                                    value={userObject.username}
-                                    onChange={handleChange}
-                                />
-                                <label htmlFor="password">Password: </label>
-                                <Input
-                                    type="password"
-                                    name="password"
-                                    value={userObject.password}
-                                    onChange={handleChange}
-                                />
-                                <Link to="/signup">Register</Link>
-                                <FormBtn onClick={handleSubmit}>Login</FormBtn>
-                            </form>
-                            {/* <NewIndex /> */}
-                        </Card>
-                    </Col>
-                    <Col size="md-3"></Col>
-                </Row>
-            </Container>
-        )
-    }
+  if (redirectTo) {
+    return <Redirect to={{ pathname: redirectTo }} />;
+  } else {
+    return (
+      <Container>
+        <Row>
+          <Col size="md-3"></Col>
+          <Col size="md-6">
+            <Card title="Login to The Web Dev Game">
+              <form style={{ marginTop: 10 }}>
+                <label htmlFor="username">Username: </label>
+                <Input
+                  type="text"
+                  name="username"
+                  value={userObject.username}
+                  onChange={handleChange}
+                />
+                <label htmlFor="password">Password: </label>
+                <Input
+                  type="password"
+                  name="password"
+                  value={userObject.password}
+                  onChange={handleChange}
+                />
+                <Link to="/signup">Register</Link>
+                <FormBtn onClick={handleSubmit}>Login</FormBtn>
+              </form>
+              {/* <NewIndex /> */}
+            </Card>
+          </Col>
+          <Col size="md-3"></Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default LoginForm;
