3216c08c131d0d80e6375b18857781d0eecf83bc
Monica Hill <monica.r.hill@gmail.com>
2021-05-01 11:50:50 -0400

    implemented a question model

---
+++ models/question.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.promise = Promise;
+
+const questionSchema = new Schema({
+  category: { type: String, unique: false, required: true },
+  subcategory: { type: String, unique: false, required: false },
+  text: { type: String, unique: false, required: true },
+  answer: { type: String, unique: false, require: true }
+
+});
+
+// Create reference to User & export
+const Question = mongoose.model('Question', questionSchema);
+module.exports = Question;
