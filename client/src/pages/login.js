2e967f2ed11924eb0d9c32ac7eb25e5cd4337bfd
Chris Risseler <chrisrisseler43@gmail.com>
2021-05-08 12:16:45 -0400

    updated terminology for username placeholders

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -54,7 +54,7 @@ function LoginForm({ login }) {
                   name="username"
                   type="text"
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-red-eggplant bg-red-linen rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="UserName"
+                  placeholder="User Name"
                   value={userObject.username}
                   onChange={handleChange}
                 />
