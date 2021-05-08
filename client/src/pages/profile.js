27a57519dde85185aa909516c496de5f6be37a57
Chris Risseler <chrisrisseler43@gmail.com>
2021-05-08 11:38:26 -0400

    created avatar generation

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
+import multiavatar from '@multiavatar/multiavatar'
 
+import Avatar from "../components/Avatar"
 
 function Profile({ user }) {
+    let svgCode = multiavatar('Binx Bond')
+    console.log(svgCode)
 
     return (
         <div>
-            <h1>Welcome {user.username}</h1>
-            <h2>Your Email: {user.email}</h2>
+            <Avatar user={user} />
+            <h1 className="text-left">Welcome: {user.username}</h1>
+            <h2 className="text-left">Your Email: {user.email}</h2>
         </div>
 
 
