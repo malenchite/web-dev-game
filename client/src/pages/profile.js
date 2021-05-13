d31da344bdf6efda0dcdf1cab3c5dad03711b905
Chris Risseler <chrisrisseler43@gmail.com>
2021-05-12 21:28:49 -0400

    fixed styling on profile page

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -30,7 +30,7 @@ function Profile({ user, setUser }) {
     function saveAvatar() {
         let randomPicture = randomGenerator();
         API.saveAvatar(user._id, randomPicture)
-          .then(() => setUser({...user, avatar: randomPicture}))
+            .then(() => setUser({ ...user, avatar: randomPicture }))
         // setUser(oldUser => {
         //     let randomPicture = randomGenerator();
         //     //oldUser.avatar = randomPicture;
@@ -41,27 +41,27 @@ function Profile({ user, setUser }) {
     }
 
     return (
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-5 gap-4">
             {user && (
                 <>
-                    <div className="shadow-xl bg-red-linen rounded-lg h-18 p-2 my-2">
+                    <div className="shadow-xl bg-red-linen rounded-lg h-18 p-2 my-2 col-start-1 col-end-2">
                         <Avatar user={user} />
                         <button className="group relative flex justify-center my-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md bg-red-mauveTaupe text-red-eggplant bg-opacity-60 hover:bg-opacity-70 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" onClick={saveAvatar} >Generate Random Avatar</button>
-                        <h1 className="text-left text-red-blackBean">Welcome: {user.username}</h1>
-                        <h2 className="text-left text-red-blackBean">Your Email: {user.email}</h2>
+                        <h1 className="text-left text-red-blackBean text-lg">Welcome: {user.username}</h1>
+                        <h2 className="text-left text-red-blackBean text-lg">Your Email: {user.email}</h2>
                     </div>
-                    <div className="shadow-xl bg-red-linen rounded-lg h-18 p-2 my-2 col-start-2 col-end-5">
-                        <h1><strong>Game History:</strong></h1>
+                    <div className="shadow-xl bg-red-linen rounded-lg h-18 p-2 my-2 col-start-2 col-end-4 px-2 h-96 overflow-y-scroll scrollbar-thin scrollbar-thumb-red-eggplant scrollbar-track-red-linen">
+                        <h1 className="text-left text-red-blackBean text-xl" ><strong>Game History:</strong></h1>
+                        <br />
                         {history.slice(0).reverse().map(({ result, frontEndCorrect, frontEndTotal, backEndCorrect, backEndTotal, timestamp }) => {
                             return (
-                                <ul>
-                                    <hr></hr>
-                                    <li>Game Result: {result}</li>
-                                    <li>Front End Points: {frontEndCorrect}/{frontEndTotal}</li>
-                                    <li>Back End Points: {backEndCorrect}/{backEndTotal}</li>
-                                    <li>Time: {format(new Date(timestamp), 'MM/dd/yyyy')}</li>
+                                <div key={timestamp} className="text-left text-red-blackBean text-lg bg-red-desertSand mb-4 mr-6">
+                                    <strong>Game Result:</strong> {result}<br />
+                                    <strong>Front End Points:</strong> {frontEndCorrect} correct out of {frontEndTotal} possible<br />
+                                    <strong>Back End Points:</strong> {backEndCorrect} correct out of {backEndTotal} possible<br />
+                                    <strong>Date Completed:</strong> {format(new Date(timestamp), 'MM/dd/yyyy')}<br />
 
-                                </ul>
+                                </div>
                             )
                         })}
                     </div>
